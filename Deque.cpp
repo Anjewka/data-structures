bc55949aa199236ac759667ad91f8ef19e0a4958
Андрей Лобанов <anjewka@gmail.com>
2024-10-22 22:47:26 +0300

    Allocator copying

--- Deque.cpp
+++ Deque.cpp
@@ -10,8 +10,8 @@ struct _Deque_iterator {
 
     typedef _Deque_iterator<T, T&, T*>              iterator;
     typedef _Deque_iterator<T, const T&, const T*>  const_iterator;
-    typedef T*                    _Type_ptr;
-    typedef T**                   _Map_ptr;
+    typedef T* _Type_ptr;
+    typedef T** _Map_ptr;
     typedef _Deque_iterator<T, _Reference, _Pointer> this_class;
 
 public:
@@ -29,12 +29,12 @@ public:
     _Type_ptr cur;
 
     _Deque_iterator() noexcept
-      : node(), first(), last(), cur() { }
+        : node(), first(), last(), cur() { }
 
     void _M_set_node(_Map_ptr __new_node) {
-	    node = __new_node;
+        node = __new_node;
         first = *__new_node;
-	    last = first + difference_type(node_size(sizeof(T)));
+        last = first + difference_type(node_size(sizeof(T)));
     }
 
     this_class& operator++() {
@@ -57,28 +57,28 @@ public:
     }
 
     this_class& operator+=(difference_type __n) noexcept {
-	    const difference_type __offset = __n + (cur - first);
+        const difference_type __offset = __n + (cur - first);
         if (__offset >= 0 && __offset < difference_type(node_size(sizeof(T))))
-        cur += __n;
+            cur += __n;
         else
         {
             const difference_type __node_offset =
-            __offset > 0 ? __offset / difference_type(node_size(sizeof(T)))
+                __offset > 0 ? __offset / difference_type(node_size(sizeof(T)))
                 : -difference_type((-__offset - 1)
-                            / node_size(sizeof(T))) - 1;
+                    / node_size(sizeof(T))) - 1;
             _M_set_node(node + __node_offset);
             cur = first + (__offset - __node_offset
-                    * difference_type(node_size(sizeof(T))));
+                * difference_type(node_size(sizeof(T))));
         }
         return *this;
-    } 
+    }
 
     this_class operator-=(size_type __n) {
-        if(__n <= cur - first) {
+        if (__n <= cur - first) {
             cur -= __n;
             return *this;
         }
-        __n -= cur - first; 
+        __n -= cur - first;
         size_type number_node = __n / node_size(sizeof(T)) + 1;
         _M_set_node(node - number_node);
         cur = last - __n % node_size(sizeof(T));
@@ -97,11 +97,11 @@ public:
     pointer operator->() { return cur; }
 
     friend bool operator==(const this_class __l, const this_class __r) {
-        return __l.cur == __r.cur; 
+        return __l.cur == __r.cur;
     }
 
     friend bool operator!=(const this_class __l, const this_class __r) {
-        return __l.cur != __r.cur; 
+        return __l.cur != __r.cur;
     }
 
     friend this_class operator+(const this_class& __r, difference_type __n) {
@@ -118,61 +118,85 @@ public:
 
     friend difference_type operator-(const this_class& __x, const this_class& __y) noexcept {
         return difference_type(node_size(sizeof(T)))
-        * (__x.node - __y.node - bool(__x.node))
-        + (__x.cur - __x.first)
-        + (__y.last - __y.cur);
+            * (__x.node - __y.node - bool(__x.node))
+            + (__x.cur - __x.first)
+            + (__y.last - __y.cur);
     }
 
     friend bool operator<(const this_class& __x, const this_class& __y) noexcept {
-	    return (__x.node == __y.node)
-	      ? (__x.cur < __y.cur) : (__x.node < __y.node);
+        return (__x.node == __y.node)
+            ? (__x.cur < __y.cur) : (__x.node < __y.node);
     }
 };
 
 template<typename T, typename _Alloc>
 class Deque_base {
 public:
-    typedef T*           _ptr;
-    typedef const T*     _const_ptr;
+    typedef T* _ptr;
+    typedef const T* _const_ptr;
     typedef std::size_t  size_type;
     typedef _Deque_iterator<T, T&, T*>             iterator;
     typedef _Deque_iterator<T, const T&, const T*> const_iterator;
     typedef typename iterator::_Map_ptr _Map_ptr;
 
-    typename _Alloc::template rebind<T>::other _allocator;
-    typedef typename _Alloc::template rebind<T>::other _alloc_type;
+    typedef typename std::allocator_traits<_Alloc>::template rebind_alloc<T> _T_alloc_type;
+    typedef std::allocator_traits<_T_alloc_type> _Alloc_traits;
 
-    typename _Alloc::template rebind<_ptr>::other _map_allocator;
-    typedef typename _Alloc::template rebind<_ptr>::other _map_alloc_type;
+    typedef typename _Alloc_traits::template rebind_alloc<_ptr> _Map_alloc_type;
+    typedef std::allocator_traits<_Map_alloc_type> _Map_alloc_traits;
 
-    using _Alloc_traits = std::allocator_traits<_alloc_type>;
-    using _Malloc_traits = std::allocator_traits<_map_alloc_type>;
+    using allocator_type = _Alloc;
 
-    struct _Impl {
+public:
+
+    allocator_type get_allocator() const noexcept {
+        return allocator_type(_M_get_Tp_allocator());
+    }
+
+    _Map_alloc_type _M_get_map_allocator() const noexcept {
+        return _Map_alloc_type(_M_get_Tp_allocator());
+    }
+
+    _T_alloc_type& _M_get_Tp_allocator() noexcept {
+        return this->_M_impl;
+    }
+
+    const _T_alloc_type& _M_get_Tp_allocator() const noexcept {
+        return this->_M_impl;
+    }
+
+    struct _Deque_data {
         _Map_ptr _M_map;
         size_type _size;
         iterator beg;
         iterator _end;
 
-        _Impl() noexcept : _M_map(), _size(), beg(), _end() {}
+        _Deque_data() noexcept : _M_map(), _size(), beg(), _end() {}
 
-        _Impl(_Impl&& __impl) : _M_map(__impl._M_map)
-                    , _size(__impl._size)
-                    , beg(__impl.beg)
-                    , _end(__impl._end) {
-            __impl._M_map = nullptr;
-            __impl._size = 0;
-            __impl.beg = iterator();
-            __impl._end = iterator();
+        _Deque_data(const _Deque_data& __impl) = default;
+
+        _Deque_data& operator=(const _Deque_data& __impl) = default;
+
+        _Deque_data(_Deque_data&& __impl) noexcept : _Deque_data(__impl) {
+            __impl = _Deque_data();
         }
-
-        _Impl(const _Impl& __impl) = default;
     };
 
-    _Impl _M_impl;
+    struct _Deque_impl : public _Deque_data, public _T_alloc_type {
+        _Deque_impl() : _T_alloc_type() {}
+
+        _Deque_impl(const _T_alloc_type& __a) noexcept : _T_alloc_type(__a) {}
+
+        _Deque_impl(_T_alloc_type&& __a) noexcept : _T_alloc_type(std::move(__a)) {}
+
+        _Deque_impl(_Deque_data&& __d, _T_alloc_type&& __a) noexcept
+            : _Deque_data(std::move(__d)), _T_alloc_type(std::move(__a)) {}
+    };
+
+    _Deque_impl _M_impl;
 
     void _M_initialize_map(size_type __n) {
-        size_type nodes_count = (__n / node_size(sizeof(T)) + 1); 
+        size_type nodes_count = (__n / node_size(sizeof(T)) + 1);
 
         _M_impl._size = std::max(static_cast<size_type>(8), size_type(nodes_count + 2));
         _M_impl._M_map = allocate_map(_M_impl._size);
@@ -182,11 +206,12 @@ public:
 
         try {
             _M_create_nodes(__nstart, __n_end);
-        } catch(...) {
-            _Malloc_traits::deallocate(_map_allocator, _M_impl._M_map, _M_impl._size);
+        }
+        catch (...) {
+            deallocate_map(_M_impl._M_map, _M_impl._size);
             _M_impl._M_map = _Map_ptr();
             _M_impl._size = 0;
-            throw ;
+            throw;
         }
 
         _M_impl.beg._M_set_node(__nstart);
@@ -196,29 +221,32 @@ public:
     }
 
     void _M_destroy_nodes(_Map_ptr __nstart, _Map_ptr __n_end) {
-        for(_Map_ptr __n = __nstart; __n < __n_end; ++__n) {
-            _Alloc_traits::deallocate(_allocator, *__n, node_size(sizeof(T)));
+        for (_Map_ptr __n = __nstart; __n < __n_end; ++__n) {
+            _Alloc_traits::deallocate(_M_impl, *__n, node_size(sizeof(T)));
         }
     }
 
     void _M_create_nodes(_Map_ptr __nstart, _Map_ptr __n_end) {
         _Map_ptr __cur;
         try {
-	        for (__cur = __nstart; __cur < __n_end; ++__cur) {
-                *__cur = _Alloc_traits::allocate(_allocator,  node_size(sizeof(T)));
+            for (__cur = __nstart; __cur < __n_end; ++__cur) {
+                *__cur = _Alloc_traits::allocate(_M_impl, node_size(sizeof(T)));
             }
-	    } catch (...) {
+        }
+        catch (...) {
             _M_destroy_nodes(__nstart, __cur);
-            throw ;
+            throw;
         }
     }
 
-    _Map_ptr allocate_map(size_type __s) {
-        return _Malloc_traits::allocate(_map_allocator, __s);
+    _Map_ptr allocate_map(size_type __n) {
+        _Map_alloc_type map_alloc = _M_get_map_allocator();
+        return _Map_alloc_traits::allocate(map_alloc, __n);
     }
 
-    void deallocate_map(_Map_ptr __p, size_type __s) {
-        _Malloc_traits::deallocate(_map_allocator, __p, __s);
+    void deallocate_map(_Map_ptr __p, size_type __n) {
+        _Map_alloc_type map_alloc = _M_get_map_allocator();
+        _Map_alloc_traits::deallocate(map_alloc, __p, __n);
     }
 
     void reallocate(size_type __add, bool __add_at_front) {
@@ -228,7 +256,7 @@ public:
 
         new_map = allocate_map(new_size);
 
-        if(!__add_at_front) {__add = 0;}
+        if (!__add_at_front) { __add = 0; }
         std::copy(_M_impl.beg.node, _M_impl._end.node + 1, new_map + __add);
 
         deallocate_map(_M_impl._M_map, prev_size);
@@ -241,10 +269,10 @@ public:
 
     template<typename... Args>
     void emplace_back(Args&&... _args) {
-        if(_M_impl._end.cur != _M_impl._end.last - 1) {
-            _Alloc_traits::construct(_allocator
-                        , _M_impl._end.cur
-                        , std::forward<Args>(_args)...);
+        if (_M_impl._end.cur != _M_impl._end.last - 1) {
+            _Alloc_traits::construct(_M_impl
+                , _M_impl._end.cur
+                , std::forward<Args>(_args)...);
             ++_M_impl._end.cur;
             return;
         }
@@ -252,28 +280,29 @@ public:
         reserve_at_back();
 
         _Map_ptr after_end = _M_impl._end.node + 1;
-        *after_end = _Alloc_traits::allocate(_allocator, node_size(sizeof(T)));
+        *after_end = _Alloc_traits::allocate(_M_impl, node_size(sizeof(T)));
 
         try {
-            _Alloc_traits::construct(_allocator
-                        , _M_impl._end.cur
-                        , std::forward<Args>(_args)...);
+            _Alloc_traits::construct(_M_impl
+                , _M_impl._end.cur
+                , std::forward<Args>(_args)...);
             _M_impl._end._M_set_node(after_end);
             _M_impl._end.cur = _M_impl._end.first;
-        } catch(...) {
+        }
+        catch (...) {
             --_M_impl._end;
-            _Alloc_traits::deallocate(_allocator, *(_M_impl._end.node + 1),
-            node_size(sizeof(T)));            
+            _Alloc_traits::deallocate(_M_impl, *(_M_impl._end.node + 1),
+                node_size(sizeof(T)));
             throw;
         }
     }
 
     template<typename... Args>
     void emplace_front(Args&&... _args) {
-        if(_M_impl.beg.cur != _M_impl.beg.first) {
-            _Alloc_traits::construct(_allocator,
-                        _M_impl.beg.cur - 1,
-                        std::forward<Args>(_args)...);
+        if (_M_impl.beg.cur != _M_impl.beg.first) {
+            _Alloc_traits::construct(_M_impl,
+                _M_impl.beg.cur - 1,
+                std::forward<Args>(_args)...);
             --_M_impl.beg.cur;
             return;
         }
@@ -281,23 +310,24 @@ public:
         reserve_at_front();
 
         _Map_ptr before_begin = _M_impl.beg.node - 1;
-        *before_begin = _Alloc_traits::allocate(_allocator, node_size(sizeof(T)));
+        *before_begin = _Alloc_traits::allocate(_M_impl, node_size(sizeof(T)));
 
         try {
             _M_impl.beg._M_set_node(before_begin);
             _M_impl.beg.cur = _M_impl.beg.last - 1;
-            _Alloc_traits::construct(_allocator
-                        , _M_impl.beg.cur
-                        , std::forward<Args>(_args)...);
-        } catch(...) {
+            _Alloc_traits::construct(_M_impl
+                , _M_impl.beg.cur
+                , std::forward<Args>(_args)...);
+        }
+        catch (...) {
             ++_M_impl.beg;
-            _Alloc_traits::deallocate(_allocator, *(_M_impl.beg.node - 1),
-            node_size(sizeof(T)));
+            _Alloc_traits::deallocate(_M_impl, *(_M_impl.beg.node - 1),
+                node_size(sizeof(T)));
             throw;
         }
     }
 
-    void push_back(const T& __x) {  
+    void push_back(const T& __x) {
         emplace_back(__x);
     }
 
@@ -313,22 +343,22 @@ public:
         emplace_front(std::move(__x));
     }
 
-    void reserve_at_front(std::size_t __add = 1)  {
-	    if(__add > size_t(_M_impl.beg.node - _M_impl._M_map)) {
+    void reserve_at_front(std::size_t __add = 1) {
+        if (__add > size_t(_M_impl.beg.node - _M_impl._M_map)) {
             reallocate(__add, true);
         }
     }
 
     void reserve_at_back(std::size_t __add = 1) {
-        if(__add + 1 > _M_impl._size - (_M_impl._end.node - _M_impl._M_map)) {
+        if (__add + 1 > _M_impl._size - (_M_impl._end.node - _M_impl._M_map)) {
             reallocate(__add, false);
         }
     }
 
     void pop_back() noexcept {
-        if(_M_impl._end.cur == _M_impl._end.first) {
-            _Alloc_traits::deallocate(_allocator, _M_impl._end.first, node_size(sizeof(T)));
-            _Alloc_traits::destroy(_allocator, _M_impl._end.cur);
+        if (_M_impl._end.cur == _M_impl._end.first) {
+            _Alloc_traits::deallocate(_M_impl, _M_impl._end.first, node_size(sizeof(T)));
+            _Alloc_traits::destroy(_M_impl, _M_impl._end.cur);
             _M_impl._end._M_set_node(_M_impl._end.node - 1);
             _M_impl._end.cur = _M_impl._end.last - 1;
             return;
@@ -336,13 +366,13 @@ public:
 
         auto last = _M_impl._end.cur;
         --_M_impl._end.cur;
-        _Alloc_traits::destroy(_allocator, last);
+        _Alloc_traits::destroy(_M_impl, last);
     }
 
     void pop_front() noexcept {
-        if(_M_impl.beg.cur == _M_impl.beg.last) {
-            _Alloc_traits::deallocate(_allocator, _M_impl.beg.first, node_size(sizeof(T)));
-            _Alloc_traits::destroy(_allocator, _M_impl.beg.cur);
+        if (_M_impl.beg.cur == _M_impl.beg.last) {
+            _Alloc_traits::deallocate(_M_impl, _M_impl.beg.first, node_size(sizeof(T)));
+            _Alloc_traits::destroy(_M_impl, _M_impl.beg.cur);
             _M_impl.beg._M_set_node(_M_impl.beg.node + 1);
             _M_impl.beg.cur = _M_impl.beg.first + 1;
             return;
@@ -350,23 +380,36 @@ public:
 
         auto _begin = _M_impl.beg.cur;
         ++_M_impl.beg.cur;
-        _Alloc_traits::destroy(_allocator, _begin);
+        _Alloc_traits::destroy(_M_impl, _begin);
     }
 
-    Deque_base() : _M_impl() {_M_initialize_map(0);}
+    Deque_base() : _M_impl() { _M_initialize_map(0); }
 
-    Deque_base(size_type __c) : _M_impl() {_M_initialize_map(__c);}
+    Deque_base(const allocator_type& __a, size_type __c) : _M_impl(__a) { _M_initialize_map(__c); }
 
-    Deque_base(const Deque_base& __d) : _M_impl() {
-        _M_initialize_map(0);
-        auto i = __d._M_impl.beg;
-        while(i != __d._M_impl._end) {
-            push_back(*i);
-            ++i;
+    Deque_base(const allocator_type& __a) : _M_impl(__a) {}
+
+    Deque_base(Deque_base&& __d) : _M_impl(std::move(__d._M_get_Tp_allocator())) {
+        __d._M_initialize_map(0);
+    }
+
+    Deque_base(Deque_base&& __d, const allocator_type& __a)
+        : _M_impl(std::move(__d._M_impl), _Tp_alloc_type(__a)) {
+        __d._M_initialize_map(0);
+    }
+
+    Deque_base(Deque_base&& __d, const allocator_type& __a, size_type __n)
+        : _M_impl(std::move(__d._M_impl), _Tp_alloc_type(__a)) {
+        if (__a == __d.get_allocator()) {
+            if (__d._M_impl._M_map) {
+                _M_initialize_map(0);
+                std::swap(__d._M_impl, _M_impl);
+            }
+            else {
+                _M_initialize_map(__n);
+            }
         }
     }
-
-    Deque_base(Deque_base&& __d) : _M_impl(std::move(__d._M_impl)) {__d._M_initialize_map(0);}
 };
 
 template<typename T, typename _Alloc = std::allocator<T>>
@@ -383,7 +426,14 @@ public:
 
     deque() = default;
 
-    deque(size_type __c) : Deque_base<T, _Alloc>(__c) {}
+    deque(const allocator_type& __a) : _base(__a) {}
+
+    deque(size_type __n, const allocator_type& __a = allocator_type()) : _base(__a, __s) {}
+
+    deque(size_type __n, const value_type& __v, const allocator_type& __a = allocator_type())
+        : _base(__a, __s) {
+
+    }
 
     deque(deque&& __d) : _base(std::move(__d)) {}
 
@@ -395,6 +445,10 @@ public:
 
     void push_back(const T& __x) {
         _base::push_back(__x);
+    }
+
+    void push_back(T&& __x) {
+        _base::push_back(std::move(__x));
     }
 
     template<typename... Args>
@@ -411,7 +465,7 @@ public:
         return *(begin() + __i);
     }
 
-    typename _base::_Impl& get_M() {return this->_M_impl;}
+    typename _base::_Deque_impl& get_M() { return this->_M_impl; }
 
     void pop_back() {
         _base::pop_back();
@@ -421,11 +475,11 @@ public:
         _base::pop_front();
     }
 
-    iterator begin() {return this->_M_impl.beg;}
+    iterator begin() { return this->_M_impl.beg; }
 
-    iterator end() {return this->_M_impl._end;}
+    iterator end() { return this->_M_impl._end; }
 
-    const_iterator cbegin() {return this->_M_impl.beg;}
+    const_iterator cbegin() { return this->_M_impl.beg; }
 
-    const_iterator cend() {return this->_M_impl._end;}
+    const_iterator cend() { return this->_M_impl._end; }
 };
